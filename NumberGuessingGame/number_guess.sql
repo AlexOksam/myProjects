aaea8770036aeaf60ea9d6a6e3794c2b745b6065
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2023-04-11 20:39:19 +0200

    Update number_guess.sql

--- NumberGuessingGame/number_guess.sql
+++ NumberGuessingGame/number_guess.sql
@@ -3,19 +3,20 @@ CREATE DATABASE number_guess;
 USE number_guess;
 
 CREATE TABLE dbo.games (
-    game_id integer PRIMARY KEY NOT NULL,
-    count_guesses integer NOT NULL,
-    user_id integer
+    game_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    count_guesses INT NOT NULL,
+    user_id INT
 );
 
+
 CREATE TABLE dbo.users (
-    user_id integer PRIMARY KEY NOT NULL,
-    username character varying(25) NOT NULL
+    user_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
 );
+
 
 ALTER TABLE dbo.users
     ADD CONSTRAINT users_username_key UNIQUE (username);
 
 ALTER TABLE dbo.games
     ADD CONSTRAINT games_user_id_fkey FOREIGN KEY (user_id) REFERENCES dbo.users(user_id);
-
